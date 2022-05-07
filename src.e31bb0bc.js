be38326ab63b95753c47ddce1da2cc08f0a370f1
Timothy1102 <kuduthang834@gmail.com>
2022-05-07 23:36:59 +0700

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -47185,7 +47185,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60852" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63404" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
