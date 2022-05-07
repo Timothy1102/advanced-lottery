f6ced942eed5ee8813af5b319c6fb02d8698d5bf
Timothy1102 <kuduthang834@gmail.com>
2022-05-08 01:57:47 +0700

    update contract, set author

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,45 +1,5 @@
-/*
- * This is an example of an AssemblyScript smart contract with two simple,
- * symmetric functions:
- *
- * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
- *    user (account_id) who sent the request
- * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
- *    defaulting to "Hello"
- *
- * Learn more about writing NEAR smart contracts with AssemblyScript:
- * https://docs.near.org/docs/develop/contracts/as/intro
- *
- */
-
+// Tim
 import { Context, logging, storage, PersistentUnorderedMap, ContractPromiseBatch, u128, RNG, util } from 'near-sdk-as'
-
-const DEFAULT_MESSAGE = 'Hello'
-
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
-}
-
-export function setGreeting(message: string): void {
-  const accountId = Context.sender
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(`Saving greeting "${message}" for account "${accountId}"`)
-  storage.set(accountId, message)
-}
-
-
-
-
-
-// Tim : starts from here 
-
-// import { context, storage, logging, PersistentMap } from "near-sdk-as";
 
 /**
  * Stores tickets balance for a list of accounts
