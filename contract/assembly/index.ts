1f30a82786bc9cc158737a120d1f67eaca4c389b
Timothy1102 <kuduthang834@gmail.com>
2022-05-07 21:33:32 +0700

    update contract

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -12,7 +12,7 @@
  *
  */
 
-import { Context, logging, storage, PersistentUnorderedMap, ContractPromiseBatch, u128 } from 'near-sdk-as'
+import { Context, logging, storage, PersistentUnorderedMap, ContractPromiseBatch, u128, RNG, util } from 'near-sdk-as'
 
 const DEFAULT_MESSAGE = 'Hello'
 
@@ -48,21 +48,12 @@ const ticketsBalance = new PersistentUnorderedMap<string, u128>("tb");
 
 const ONE_NEAR = u128.from("1000000000000000000000000");
 
+
 /**
  * convert yocto Ⓝ (1e24) to NEAR
  */
 function asNEAR(amount: u128): u128 {
   return u128.div(amount, ONE_NEAR);
-}
-
-
-/**
- * util function: transfer 200 NEAR from dev account to an existing account in case the account is running out of NEAR
- */
- export function transferNEAR(): void {
-  const TWO_HUNDRED_NEAR = u128.from("200000000000000000000000000");
-  const receiver = ContractPromiseBatch.create("timthang.testnet");
-  receiver.transfer(TWO_HUNDRED_NEAR);
 }
 
 
@@ -87,7 +78,7 @@ export function buyTickets(): void {
   const amount = asNEAR(Context.attachedDeposit);
 
   if(storage.getString("pool") == null) {
-    storage.set<u128>("pool", u128.from(0));
+    storage.set<u128>("pool", amount);
   } else {
     let current_pool = storage.getSome<u128>("pool");
     storage.set<u128>("pool", u128.add(current_pool, amount));
@@ -113,12 +104,140 @@ export function getPool(): u128 {
   return res;
 }
 
-/**
- * calculate the chance for every account to win the lottery, weight-based
- */
-function play(): void {
-  const total_tickets = u128.mul(getPool(), u128.from(10));
-  
 
+export function getTotalTickets(): u32 {
+  const total: string= u128.mul(getPool(), u128.from(10)).toString();
+  const total_tickets: u32 = util.parseFromString<u32>(total);
+  return total_tickets;
 }
 
+
+/**
+ * calculate the chance for every account to win the lottery, weight-based
+ * determine the winner
+ */
+export function play(): void {
+  const account_list = ticketsBalance.keys();
+  logging.log("THE LOTTERY CURRENTLY HAS : " + account_list.length.toString() + " PLAYERS");
+  logging.log("TOTAL TICKETS = " + getTotalTickets().toString());
+  logging.log("TOTAL POOL = " + getPool().toString());
+  logging.log("+++++++++++++++++++++++++++++++++++++++++++++");
+
+  while(storage.get<string>("winner") == null) {
+    ticketsBalance.keys().forEach(account => {
+      const arr = chance();
+      let threshold = util.parseFromString<u32>(arr[0].toString());
+      let bonus = util.parseFromString<u32>(arr[1].toString());
+      let top = util.parseFromString<u32>(arr[2].toString());
+      let number_of_top = util.parseFromString<u32>(arr[3].toString());
+
+      let tickets: string = ticketsBalance.getSome(account).toString();
+      let tickets_in_u32: u32 = util.parseFromString<u32>(tickets);
+
+      logging.log(account + " has " + tickets + " tickets")
+
+      if(!(tickets_in_u32 < threshold)) {
+        const rng = new RNG<u32>(1, getTotalTickets());
+        const roll = rng.next();
+        // logging.log(account + " has " + tickets + " tickets...")
+        logging.log("roll: " + roll.toString());
+        if(tickets_in_u32 == top) {
+          logging.log("VIP: + " + (bonus/number_of_top).toString() + " TICKETS");
+          if(roll <= (tickets_in_u32 + bonus/number_of_top) ){
+            if(storage.get<string>("winner") == null){
+              storage.set("winner", account);
+            }
+          }
+        } 
+        else if(roll <= tickets_in_u32){
+          if(storage.get<string>("winner") == null){
+            storage.set("winner", account);
+          }
+        }
+      }
+
+    });
+  }
+  // get winner 
+  if (!storage.get<string>("winner") == null) {
+    logging.log("=====================++++++++++================");
+    logging.log("WINNER: " + storage.getSome<string>("winner"));
+    logging.log("=====================++++++++++================");
+  }
+}
+
+
+/**
+ * reset winner
+ */
+export function resetWinner(): void {
+  storage.delete("winner");
+}
+
+/**
+ * reset all
+ */
+ export function reset(): void {
+  storage.delete("winner");
+  storage.delete("pool");
+  ticketsBalance.clear();
+}
+
+
+export function getWinner(): string {
+  if (!storage.get<string>("winner") == null) {
+    return storage.getSome<string>("winner");
+  } else {
+    return "NOBODY HAS WON";
+  }
+}
+
+
+/**
+ * send 95% pool to the winner, the host (contract account) keeps 5% pool.
+ * reset the lottery.
+ */
+export function sendReward(): void {
+  const receiver = ContractPromiseBatch.create(getWinner());
+  const reward_pool: u128 = u128.mul(getPool(), ONE_NEAR);
+  const reward_to_winner: u128 = u128.div(u128.mul(reward_pool, u128.from(95)), u128.from(100))
+  receiver.transfer(reward_to_winner);
+  reset();
+}
+
+
+/**
+ * array[0]: if the tickets balance of an account is less than this value, that account will not take part in the lottery.
+ * array[1]: total bonus (the top 5% lowest tickets balance) to the #1 account with highest tickets balance.
+ * array[2]: current highest tickets balance accross all accounts.
+ * array[3]: number of account with the same highest tickets balance (if any), the bonus will be splitted to those accounts.
+ * 
+ * note: in case there is no account with balance less than 5% total tickets, 
+ * the account with lowest balance will be rejected to take part in the lottery and its balance will be the bonus.
+ */
+export function chance(): u128[] {
+  const array = new Array<u128>(4);
+  array[3] = u128.from(0);
+  const sorted_tickets_balance = ticketsBalance.values().sort();
+  const total_tickets = getTotalTickets();
+  const threshold = u128.from(total_tickets/20); //bottom 5% ticket will not be counted
+  // logging.log("THRESHOLD = " + threshold.toString());
+  var a: u128 = u128.from(0);
+  for(let i = 0; i < sorted_tickets_balance.length; i++) {
+    if(a <= threshold) {
+      a = u128.add(a, sorted_tickets_balance.shift());
+      array[1] = a;
+    }
+  }
+
+  for(let i = 0; i < sorted_tickets_balance.length; i++) {
+    if(sorted_tickets_balance[i] = sorted_tickets_balance[sorted_tickets_balance.length-1]) {
+      array[3] = u128.add(array[3], u128.from(1));
+    }
+  }
+
+  array[0] = sorted_tickets_balance[0];
+
+  array[2] = sorted_tickets_balance.pop();
+  return array;
+}
